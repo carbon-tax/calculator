b23305c9ad9db7acee35ab6231bcf311452d07db
Timo Uhlmann <timouhlmann@gmail.com>
2018-11-02 09:20:40 +0100

    React setup

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { hot } from 'react-hot-loader'
+
+class App extends Component {
+  render () {
+    return (
+    <>
+      <h1>Carbon Tax Calculator</h1>
+      <p>Lorem ipsum!</p>
+    </>
+    )
+  }
+} export default hot(module)(App)
