162fe2d3d874f6fd956664c7c79b770ec20abe9b
Timo Uhlmann <timouhlmann@gmail.com>
2018-11-25 14:27:25 +0100

    FontAwesome icons

---
+++ src/components/Icon.js
@@ -0,0 +1,9 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+import { } from '@fortawesome/free-brands-svg-icons'
+import { } from '@fortawesome/free-regular-svg-icons'
+
+library.add()
+
+export default FontAwesomeIcon
