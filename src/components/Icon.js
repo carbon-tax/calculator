71da35d702c75ffb4540eaa0fb47498f51be7229
Timo Uhlmann <timouhlmann@gmail.com>
2018-12-19 21:18:49 +0100

    Share icon

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -1,9 +1,14 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-import { } from '@fortawesome/free-brands-svg-icons'
-import { } from '@fortawesome/free-regular-svg-icons'
+import {
+  faLink,
+  faShareSquare
+} from '@fortawesome/free-solid-svg-icons'
 
-library.add()
+library.add(
+  faLink,
+  faShareSquare
+)
 
 export default FontAwesomeIcon
